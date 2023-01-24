c35871ad5078bff5b51104c6f3519d4eab97688a
Aniket Sharma <aniketsharma312@gmail.com>
2023-01-23 17:57:23 -0700

    more changes to the website, adding of button on intro

--- src/Main/Components/Introduction.js
+++ src/Main/Components/Introduction.js
@@ -3,7 +3,6 @@ import classes from './Introduction.module.css'
 import SelfImage from '../../assets/aniket-sharma.webp'
 import LinkedIn from "./TechIcons/Linkedin";
 import GitHub from "./TechIcons/Github";
-import MailAdd from "./TechIcons/Email";
 
 const Introducton = (props) => {
     return (
@@ -27,10 +26,12 @@ const IntroductionText = (props) => {
             <span>Hello, My Name Is</span> 
             <h1>Aniket Sharma</h1>
             <p>I am a developer, designer, and marketer. I love to create using Javascript on both frontend and backend,as well as helping small businesses by improving their digital footprint all while continuously learning.</p>
+            <div className={classes.contact}>
+             <a className={classes['contact-button']} href="https://anikets.dev/" onClick={props.contactFunction}>Contact Me</a>
+            </div>
             <div>
                 <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/andy-sharma-554b03212/'><LinkedIn /></a>
                 <a target="_blank" rel="noreferrer" href='https://github.com/AniketS312'><GitHub /></a>
-                <a href="https://anikets.dev/" onClick={props.contactFunction}><MailAdd /></a>
             </div>
         </>
     )
